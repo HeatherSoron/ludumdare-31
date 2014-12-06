b060808048f70c33575397d304a175c827781b7e
Ethan "Soron" Kaminski <ethan.kaminski@gmail.com>
2014-12-06 17:26:57 +1300

    Add platforms to catch the player
    Also fixed a bug involving the color order being wrong when cycling down.

--- js/main.js
+++ js/main.js
@@ -10,10 +10,17 @@ var colors = [
 	new Color(0, 0, 255, alpha),
 ];
 
+var platforms = [
+];
+
 var keys = {
 	'a': 65,
 	'd': 68,
 	's': 83,
+	
+	
+	'left': 37,
+	'right': 39,
 	
 	'up': 38,
 	'down': 40,
@@ -32,22 +39,59 @@ player.draw = function() {
 player.colorDown = function() {
 	player.color--;
 	while (player.color < 0) {
-		player.color = colors.length - player.color;
+		player.color += colors.length;
 	}
+}
+
+player.moveLeft = function() {
+	this.x -= 1;
+}
+player.moveRight = function() {
+	this.x += 1;
+}
+
+player.grounded = function() {
+	for (var y in platforms) {
+		if (y > this.y && y <= this.y + this.height) {
+			for (var i in platforms[y]) {
+				var platform = platforms[y][i];
+				if (platform.right > this.x && platform.left < this.x + this.width) {
+					return true;
+				}
+			}
+		}
+	}
+	return false;
 }
 
 function init() {
 	canvas = document.getElementById("game");
 	ctx = canvas.getContext("2d");
+	ctx.lineWidth = 4;
 	
 	registerListeners();
+	
+	new Platform(0, canvas.width, canvas.height, new Color(0,0,0,1));
 	
 	gameLoop = setInterval(runGame, 25);
 }
 
 function runGame() {
+	if (!player.grounded()) {
+		player.y += 2;
+	}
+	render();
+}
+
+function render() {
 	clearCtx();
 	player.draw();
+	for (var y in platforms) {
+		for (var j = 0; j < platforms[y].length; ++j) {
+			platform = platforms[y][j];
+			platform.draw();
+		}
+	}
 }
 
 function clearCtx() {
@@ -63,6 +107,14 @@ function handleKeyDown(e) {
 	document.getElementById("keycode").innerHTML = e.keyCode;
 	
 	switch (e.keyCode) {
+		case keys.left:
+			player.moveLeft();
+			e.preventDefault();
+			break;
+		case keys.right:
+			player.moveRight();
+			e.preventDefault();
+			break;
 		case keys.up:
 			player.color++;
 			e.preventDefault();
@@ -91,6 +143,30 @@ function drawTestShapes() {
 	ctx.beginPath()
 	ctx.rect(50, 50, 60, 30);
 	ctx.fill();
+}
+
+
+
+function Platform(left, right, y, color) {
+	this.left = left;
+	this.right = right;
+	this.y = y;
+	this.color = color;
+	
+	// register the platform in the global list
+	if (!platforms[y]) {
+		platforms[y] = [];
+	}
+	
+	platforms[y].push(this);
+}
+
+Platform.prototype.draw = function() {
+	ctx.strokeStyle = this.color.toString();
+	ctx.beginPath();
+	ctx.moveTo(this.left, this.y);
+	ctx.lineTo(this.right, this.y);
+	ctx.stroke();
 }
 
 
