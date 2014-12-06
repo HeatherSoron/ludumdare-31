70f5ff38f1aabd9f8059f5d247f9b502f40cc9e3
Ethan "Soron" Kaminski <ethan.kaminski@gmail.com>
2014-12-06 19:31:27 +1300

    Reorganize source files a bit more

--- js/main.js
+++ js/main.js
@@ -2,9 +2,6 @@ var gameLoop;
 
 var fallSpeed = 4;
 
-var platforms = [
-];
-
 
 function init() {
 	canvas = document.getElementById("game");
@@ -38,38 +35,4 @@ function runGame() {
 	}
 	
 	render();
-}
-
-
-function Platform(left, right, y, color) {
-	this.left = left;
-	this.right = right;
-	this.y = y;
-	this.color = color;
-	
-	// register the platform in the global list
-	if (!platforms[y]) {
-		platforms[y] = [];
-	}
-	
-	platforms[y].push(this);
-	console.log("Placing platform", this);
-}
-
-Platform.prototype.trySpawn = function() {
-	if ((Math.random() * 500) < 1) {
-		var width = (this.right - mobSize) - this.left;
-		if (width < 0) {
-			width = 0;
-		}
-		mobs.push(new Mob(this.left + Math.random() * width, this.y - mobSize, this.color));
-	}
-}
-
-Platform.prototype.draw = function() {
-	ctx.strokeStyle = this.color.toString();
-	ctx.beginPath();
-	ctx.moveTo(this.left, this.y);
-	ctx.lineTo(this.right, this.y);
-	ctx.stroke();
 }
