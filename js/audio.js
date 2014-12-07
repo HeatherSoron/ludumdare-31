11bbf0274235c5431af7f3837d2de2f04a2e1e45
Ethan "Soron" Kaminski <ethan.kaminski@gmail.com>
2014-12-07 18:30:55 +1300

    Disable audio in Safari (laggy), and inform the user

--- js/audio.js
+++ js/audio.js
@@ -1,11 +1,21 @@
 var sfx = {};
 
+var disableAudio = false;
+
 function loadAudio() {
 	createSfx('attack');
 	createSfx("failure");
 	createSfx("jump");
 	createSfx("create");
 	createSfx("powerup");
+	
+	if (navigator.userAgent.indexOf("Safari") != -1 && navigator.userAgent.indexOf("Chrome") == -1) {
+		disableAudio = true;
+		var li = document.createElement("li");
+		li.innerHTML = '<span class="ctrl">Audio is disabled in Safari</span>, due to an annoying audio lag issue. Reload in a different browser for audio. Or, if you want to hack the JavaScript, run "disableAudio = false" in the JS console.';
+		var ul = document.getElementById("tips");
+		ul.insertBefore(li, document.getElementById("first-tip"));
+	}
 }
 
 function createSfx(name) {
@@ -14,6 +24,8 @@ function createSfx(name) {
 }
 
 function playSfx(name) {
-	console.log(sfx);
+	if (disableAudio) {
+		return;
+	}
 	sfx[name].play();
 }
